622d608db4c88acf29dd262ab6969fb7a1b10221
권강빈 <105830988+kwonkangbin@users.noreply.github.com>
2023-02-03 11:20:38 +0900

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,3 +1,14 @@
 from utlis import *
 
 myDrone = initializeTello()
+
+w, h = 360, 240
+
+while True:
+    img = telloGetFrame(myDrone, w, h)
+    cv2.imshow('Image', img)
+    if cv2.waitKey(1) & 0xff == 27:
+        myDrone.land()
+        break
+
+#djitellopy 1.5 numpy 1.19.0 opencv-python 4.3.0.36
