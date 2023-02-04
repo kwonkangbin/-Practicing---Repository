396fde6dafe5b8af4c5ce77d4a2835d9d9679f5c
권강빈 <105830988+kwonkangbin@users.noreply.github.com>
2023-02-04 11:07:41 +0900

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,14 +1,25 @@
+import numpy as np
+
 from utlis import *
 
 myDrone = initializeTello()
-
+pid = [0.5, 0.5, 0]
 w, h = 360, 240
+pError = 0
+startCounter = 1#0으로 바꾸면 난다
 
 while True:
+    if startCounter == 0:
+        myDrone.takeoff()
+        startCounter = 1
     img = telloGetFrame(myDrone, w, h)
+    img, info = findFace(img)
+    #print(info)
+    pError = trackFace(myDrone, info, w, pid, pError)
     cv2.imshow('Image', img)
     if cv2.waitKey(1) & 0xff == 27:
         myDrone.land()
         break
+
 
 #djitellopy 1.5 numpy 1.19.0 opencv-python 4.3.0.36
