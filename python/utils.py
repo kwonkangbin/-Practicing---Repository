01f311965399e3db0c06dacafe095499293b0e4a
권강빈 <105830988+kwonkangbin@users.noreply.github.com>
2023-02-04 11:08:01 +0900

    Update utils.py

--- python/utils.py
+++ python/utils.py
@@ -1,4 +1,5 @@
 from djitellopy import Tello
+import numpy as np
 import cv2
 
 def initializeTello():
@@ -22,3 +23,52 @@ def telloGetFrame(myDrone, w = 360, h = 240):
     myFrame = myFrame.frame
     img = cv2.resize(myFrame, (w,h))
     return img
+
+def findFace(img):
+    faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(imgGray, 1.2, 4)
+
+    myFaceListArea = []
+    myFaceListCenter = []
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y) + (x + w, y + h), (255, 0, 0), 2)
+
+        cx = x + w // 2
+        cy = y + h // 2
+        area = w * h
+
+        myFaceListArea.append(area)
+        myFaceListCenter.append((cx, cy))
+
+    if len(myFaceListArea) != 0:
+        index = myFaceListArea.index(max(myFaceListArea))
+        return img, [myFaceListCenter[index], myFaceListArea[index]]
+
+    else:
+        return img, [[0,0],0]
+
+
+def trackFace(myDrone, info, w, pid, pError):
+    # PID
+    error = info[0][0] - w // 2
+    speed = pid[0] * error + pid[1] * (error - pError)
+    speed = int(np.clip(speed, -100, 100))
+    print(speed)
+
+    if info[0][0] != 0:
+        myDrone.yaw_velocity = speed
+    else:
+        myDrone.yaw_velocity = 0
+        myDrone.for_back_velocity = 0
+        myDrone.left_right_velocity = 0
+        myDrone.up_down_velocity = 0
+        error = 0
+    if myDrone.send_rc_control:
+        myDrone.send_rc_control(myDrone.for_back_velocity,
+                                myDrone.left_right_velocity,
+                                myDrone.up_down_velocity,
+                                myDrone.yaw_velocity)
+    return error
